8f20bb1ad560517db0e3fde80267da730f404c5e
Jason Proctor <jason@redfish.net>
2026-05-25 14:37:58 -0700

    addin' the tdc

---
+++ the-drinking-classes/update.sh
@@ -0,0 +1,8 @@
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "I Killed Him For You" -c 1 01.IKilledHimForYou.mp3
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "Suicide Mission" -c 2 02.SuicideMission.mp3
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "You Needed Space" -c 3 03.YouNeededSpace.mp3
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "Hello, Gorgeous" -c 4 04.HelloGorgeous.mp3
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "Let Alone" -c 5 05.LetAlone.mp3
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "Problem Solved" -c 6 06.ProblemSolved.mp3
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "Getting Warmer" -c 7 07.GettingWarmer.mp3
+id3tool -r TDC -a "Scourge of the Drinking Classes" -t "Last Call" -c 8 08.LastCall.mp3
